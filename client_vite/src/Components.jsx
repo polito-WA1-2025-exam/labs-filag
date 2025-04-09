b595411460f6672a25119a8be079010cdc53954a
elisa0205 <elisa.bariselli02@gmail.com>
2025-04-09 11:40:57 +0200

    Final traslation

--- client_vite/src/Components.jsx
+++ client_vite/src/Components.jsx
@@ -5,22 +5,19 @@ import Button from 'react-bootstrap/Button';
 import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
 
-import slide1 from './assets/images/Poke1.jpg';
-import slide2 from './assets/images/Poke2.jpeg';
-import slide3 from './assets/images/Poke3.jpg';
 import './App.css';
 
 
 
-export function NavBar({lang, handleLang}) {
+export function NavBar({lang, handleLang, text}) {
   return (
     <>
       <Navbar className='navbar-custom' >
         <Container>
-          <Navbar.Brand href="#home">Poke House</Navbar.Brand>
+          <Navbar.Brand href="#home">{text.title}</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#ingredients">Ingredients</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#ingredients">{text.ingredients}</Nav.Link>
+            <Nav.Link href="#pricing">{text.prices}</Nav.Link>
           </Nav>
           <Button variant="light" onClick={handleLang}><i class="bi bi-translate"> {lang} </i></Button>
         </Container>
@@ -30,64 +27,53 @@ export function NavBar({lang, handleLang}) {
 }
 
 
-export function CarouselPoke() {
+export function CarouselPoke({ text }) {
+
   return (
     <Carousel className="carousel-custom">
-      <Carousel.Item>
-        <img className="d-block w-100" src={slide3} alt="First slide"/>
-        <Carousel.Caption>
-          <h3>Time for a Bowl?</h3>
-          <p>Choose from the many available ingredient options</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={slide2} alt="Second slide"/>
-        <Carousel.Caption>
-          <h3>Try the bowl of the month</h3>
-          <p>Venere rice, salmon, edamame, tuna sauce, and cashews</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={slide1} alt="Third slide"/>
-        <Carousel.Caption>
-          <h3>Student discount</h3>
-          <p> 20% discount on all bowls if you're a university student</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+      {text.map((slide, index) => (
+        <Carousel.Item key={index}>
+          <img className="d-block w-100" src={slide.img} alt={`Slide ${index + 1}`} />
+          <Carousel.Caption>
+            <h3>{slide.title}</h3>
+            <p>{slide.description}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 }
 
-export function MyCard({img, title, text}) {
-  const imageSrc = img === '1' ? slide1 : img === '2' ? slide2 : slide3;
-
+export function MyCard({text}) {
   return (
-    <Card className='card-custom'>
-      <Card.Img variant="top" src={imageSrc} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>
-          {text}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <div className='card-container'>
+      {text.map((card, index) => (
+        <Card key={index} className='card-custom'>
+          <Card.Img variant="top" src={card.img} />
+          <Card.Body>
+            <Card.Title>{card.title}</Card.Title>
+            <Card.Text>{card.price}</Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
   );
 }
 
-export function Footer() {
+export function Footer({text}) {
   return (
     <footer className="footer-custom">
-      <span> FilaG Spa </span>
-      <span> © 2025 Poke House</span>
+      <span> {text.group} </span>
+      <span>{text.footer}</span>
     </footer>
   );
 }
 
-export function Title() {
+export function Title({text}) {
   return (
     <div className="title-custom">
-        <h1 >Choose your bowl size</h1>
-        <p >Select one of the following size and then select your personal ingredients from our proposal</p>
+        <h1 >{text.introQuestion}</h1>
+        <p >{text.introDescription}</p>
     </div>
   );
 }
