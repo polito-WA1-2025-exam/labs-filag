051cae7c577f63bbfc7d679db0f532cd1d4f7c98
Shuba <basciu36@gmail.com>
2025-05-17 14:00:03 +0200

    delete

--- User.mjs
+++
@@ -1,6 +0,0 @@
-function User(name, surname, email, birthdate){
-    this.name = name;
-    this.surname = surname;
-    this.email = email;
-    this.birthdate = birthdate;
-    }
--- db.mjs
+++
@@ -1,7 +0,0 @@
-import sqlite from 'sqlite3'; 
-
-const db = new sqlite.Database('poke.sqlite', (err) => { 
-    if (err) throw err; 
-});
-
-export default db;
